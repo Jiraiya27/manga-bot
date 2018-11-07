d65199d55c0f407298579f73df31b497a6a8b11f
Jiraiya27 <jiraiya279@gmail.com>
2018-11-08 00:49:37 +0700

    Fix RoomFeeds missing primary key

--- src/entities/RoomFeeds.ts
+++ src/entities/RoomFeeds.ts
@@ -1,16 +1,16 @@
-import { BaseEntity, Entity, Column, ManyToOne } from 'typeorm'
+import { BaseEntity, Entity, PrimaryColumn, Column, ManyToOne } from 'typeorm'
 import { Room } from './Room'
 import { Feed } from './Feed'
 
 
 @Entity()
 export class RoomFeeds extends BaseEntity{
-  @Column({ type: 'text' })
+  @PrimaryColumn({ type: 'text' })
   roomId: string
   @ManyToOne(type => Room, room => room.roomFeeds, { primary: true, onDelete: 'CASCADE' })
   room: Room
 
-  @Column({ type: 'text' })
+  @PrimaryColumn({ type: 'text' })
   feedId: string
   @ManyToOne(type => Feed, feed => feed.roomFeeds, { primary: true, onDelete: 'CASCADE' })
   feed: Feed
