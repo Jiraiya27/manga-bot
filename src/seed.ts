1fcfa55544a558ae2e9f847131b960d6b55d1a3e
Jiraiya27 <jiraiya279@gmail.com>
2018-11-08 00:54:04 +0700

    comment out migrate script
    to fix tsc and leave it for now until migration is stable

--- src/seed.ts
+++ src/seed.ts
@@ -1,35 +1,35 @@
-import app from './app'
-import { Feed } from './entities/Feed';
-import { Room } from './entities/Room';
-// import { RoomFeeds } from './entities/RoomFeeds';
+// import app from './app'
+// import { Feed } from './entities/Feed';
+// import { Room } from './entities/Room';
+// // import { RoomFeeds } from './entities/RoomFeeds';
 
-import rooms from './rooms.json'
-import rsschannels from './rss_channels.json';
+// import rooms from './rooms.json'
+// import rsschannels from './rss_channels.json';
 
-app.on('started', async () => {
-  await Promise.all(rsschannels.map(rssChannel => {
-    const item = rssChannel.items[0]
-    const feed = Feed.create({
-      source: rssChannel.src,
-      title: rssChannel.title,
-      frequency: rssChannel.frequency,
-      global: rssChannel.global,
-      lastUpdated: rssChannel.lastUpdated.$date,
-      lastItem: {
-        title: item.title,
-        link: item.link,
-        pubDate: item.pubDate,
-        isoDate: item.isoDate.$date,
-        content: item.content,
-      }
-    })
-    return feed.save()
-  }))
-  return rooms.map(room => {
-    const r = Room.create({
-      id: room.id,
-      type: room.type as 'user' | 'room' | 'group',
-    })
-    return r.save()
-  })
-})
+// app.on('started', async () => {
+//   await Promise.all(rsschannels.map(rssChannel => {
+//     const item = rssChannel.items[0]
+//     const feed = Feed.create({
+//       source: rssChannel.src,
+//       title: rssChannel.title,
+//       frequency: rssChannel.frequency,
+//       global: rssChannel.global,
+//       lastUpdated: rssChannel.lastUpdated.$date,
+//       lastItem: {
+//         title: item.title,
+//         link: item.link,
+//         pubDate: item.pubDate,
+//         isoDate: item.isoDate.$date,
+//         content: item.content,
+//       }
+//     })
+//     return feed.save()
+//   }))
+//   return rooms.map(room => {
+//     const r = Room.create({
+//       id: room.id,
+//       type: room.type as 'user' | 'room' | 'group',
+//     })
+//     return r.save()
+//   })
+// })
