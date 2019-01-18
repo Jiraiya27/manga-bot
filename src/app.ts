28b1c78f7598384a770fe6eac17094becddb19be
Jiraiya27 <jiraiya279@gmail.com>
2019-01-18 11:25:57 +0700

    synchronize false to prevent data loss

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ createConnection({
   extra: {
     ssl: true,
   },
-  synchronize: true,
+  synchronize: false,
   entities: [path.join(__dirname, '/entities/**/*{.js,.ts}')],
   migrations: [path.join(__dirname, '/migrations/**/*{.js,.ts}')],
 })
