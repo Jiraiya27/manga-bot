4454a165066c10eeb8d8ed8d1c99cce6c5ff488e
Jiraiya27 <jiraiya279@gmail.com>
2019-02-01 16:29:16 +0700

    roomFeed onUpdate cascade

--- src/entities/RoomFeeds.ts
+++ src/entities/RoomFeeds.ts
@@ -6,12 +6,12 @@ import { Feed } from './Feed'
 export class RoomFeeds extends BaseEntity {
   @PrimaryColumn({ type: 'text' })
   roomId: string
-  @ManyToOne(type => Room, room => room.roomFeeds, { primary: true, onDelete: 'CASCADE' })
+  @ManyToOne(type => Room, room => room.roomFeeds, { primary: true, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   room: Room
 
   @PrimaryColumn({ type: 'text' })
   feedId: string
-  @ManyToOne(type => Feed, feed => feed.roomFeeds, { primary: true, onDelete: 'CASCADE' })
+  @ManyToOne(type => Feed, feed => feed.roomFeeds, { primary: true, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   feed: Feed
 
   @Column({ type: 'json', default: [] })
